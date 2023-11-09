abde76f3f873471ff836059fada4aec7221469e5
Prasanta Bhuyan <prasanta.k.bhuyan@gmail.com>
2023-11-08 19:39:57 -0500

    Miscellaneous changes

--- order-service/src/main/java/com/example/microservice/order/config/WebClientConfig.java
+++ order-service/src/main/java/com/example/microservice/order/config/WebClientConfig.java
@@ -3,7 +3,6 @@ package com.example.microservice.order.config;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration

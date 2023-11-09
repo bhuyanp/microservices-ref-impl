f1417dc144c238359a5e14a051e6084f56867890
Prasanta Bhuyan <prasanta.k.bhuyan@gmail.com>
2023-11-09 13:18:12 -0500

    WebMvc not needed at root project

--- src/main/java/com/example/microservice/MicroserviceApplication.java
+++ src/main/java/com/example/microservice/MicroserviceApplication.java
@@ -2,10 +2,8 @@ package com.example.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@EnableWebMvc
 public class MicroserviceApplication {
 
 	public static void main(String[] args) {

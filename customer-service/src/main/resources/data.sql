fe2b70f1b3ac8ed0500498de6833cd01bd48b9fc
Prasanta Bhuyan <prasanta.k.bhuyan@gmail.com>
2023-11-08 19:36:44 -0500

    Reordered modules

--- customer-service/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-insert into customer (first_name, last_name, email)
-  VALUES
-  ('Prasanta','Bhuyan','prasanta.k.bhuyan@gmail.com'),
-  ('PK','Bhuyan','bhuyanp@gmail.com'),
-  ('PKB','Bhuyan','test@gmail.com')
-  ;
